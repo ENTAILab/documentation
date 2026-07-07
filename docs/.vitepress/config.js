8298202d37301bb7e661a83497e49b542f994b70
Mevluet Bagci <bagci@em.uni-frankfurt.de>
2026-07-07 09:15:58 +0200

    Fix Bug of Navbar Linking

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,10 +16,10 @@ export default {
 
     nav: [
       { text: "Home", link: "/" },
-      { text: "Measure 1", link: "measure-1/" },
-      { text: "Measure 2", link: "measure-2/" },
-      { text: "Measure 3", link: "measure-3/" },
-      { text: "Measure 4", link: "measure-4/" },
+      { text: "Measure 1", link: "/measure-1/" },
+      { text: "Measure 2", link: "/measure-2/" },
+      { text: "Measure 3", link: "/measure-3/" },
+      { text: "Measure 4", link: "/measure-4/" },
       { text: "About", link: "https://www.new-data-spaces.de/en-us/Start/About-the-Programme/ENTAILab" }
     ],
 
