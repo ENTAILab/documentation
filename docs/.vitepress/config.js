7261c706e7c56af122da798179b1a7f5f707a668
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2026-07-08 12:58:06 +0200

    Update config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -16,14 +16,14 @@ export default {
 
     nav: [
       { text: '<i class="fa-solid fa-house"></i> Home', link: "/" },
-      { text: "<i class="fa-solid fa-flask"></i> Measure 1", link: "/measure-1/" },
-      { text: "<i class="fa-solid fa-flask"></i> Measure 2", link: "/measure-2/" },
-      { text: "<i class="fa-solid fa-flask"></i> Measure 3", link: "/measure-3/" },
-      { text: "<i class="fa-solid fa-flask"></i> Measure 4", link: "/measure-4/" },
+      { text: '<i class="fa-solid fa-flask"></i> Measure 1', link: "/measure-1/" },
+      { text: '<i class="fa-solid fa-flask"></i> Measure 2', link: "/measure-2/" },
+      { text: '<i class="fa-solid fa-flask"></i> Measure 3', link: "/measure-3/" },
+      { text: '<i class="fa-solid fa-flask"></i> Measure 4', link: "/measure-4/" },
       {
         text: '<i class="fa-brands fa-github"></i> GitHub',
         link: 'https://github.com/ENTAILab'
-      }
+      },
       { text: "About", link: "https://www.new-data-spaces.de/en-us/Start/About-the-Programme/ENTAILab" }
     ],
 
